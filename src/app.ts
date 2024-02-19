74a4bd2e250c7193a0e0db6cdbbdf6edc8bd5d9f
KimCookieYa <min49590@gmail.com>
2024-02-19 13:02:20 +0900

    chore: nodecron에서 github actions cron으로 변경

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,17 @@
-import Koa from "koa";
-import cron from "node-cron";
 import dotenv from "dotenv";
 
 import { crawlWebsite } from "./utils/job";
 
 dotenv.config();
 
-const app = new Koa();
+async function main() {
+  // 매일 대한민국 오후 8시에 실행
+  console.log("크롤링 작업을 시작합니다.");
+  await crawlWebsite();
+  console.log("크롤링 작업이 종료되었습니다.");
+}
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World";
-});
-
-app.listen(3000, () => {
-  console.log("Koa server is running!!");
-  crawlWebsite();
-});
-
-cron.schedule(
-  "0 20 * * *",
-  () => {
-    // 매일 대한민국 오후 8시에 실행
-    console.log("크롤링 작업을 시작합니다.");
-    crawlWebsite();
-    console.log("크롤링 작업이 종료되었습니다.");
-  },
-  {
-    scheduled: true,
-    timezone: "Asia/Seoul",
-  }
-);
+(async () => {
+  await main();
+  process.exit(0);
+})();
