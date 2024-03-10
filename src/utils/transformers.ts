c72c3c64b7369c84ae9e095c4dfeeac2e3852868
KimCookieYa <min49590@gmail.com>
2024-03-10 19:31:03 +0900

    feat: 동적 임포팅 수정

--- src/utils/transformers.ts
+++ src/utils/transformers.ts
@@ -1,4 +1,5 @@
 import {
+  pipeline,
   PipelineType,
   ZeroShotClassificationOutput,
   ZeroShotClassificationPipeline,
@@ -16,7 +17,6 @@ export class MyZeroShotClassificationPipeline {
       console.log(
         "학습된 pipeline을 찾을 수 없습니다. 새로운 pipeline을 생성합니다."
       );
-      const { pipeline } = await import("@xenova/transformers");
       this.instance = (await pipeline(this.task, this.model, {
         progress_callback,
       })) as unknown as ZeroShotClassificationPipeline;
